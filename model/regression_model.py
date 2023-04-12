1178f78e1e267d0717f00538ce8bda298516c5d2
Shibu Mohapatra <44550746+MohapatraShibu@users.noreply.github.com>
2023-04-12 11:18:21 +0530

    regression commit 2

--- model/regression_model.py
+++ model/regression_model.py
@@ -9,10 +9,14 @@ import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
 def get_model(df,target):
-    setup(df,target=target)
+    setup(df,target=target,silent=True)
     setup_df = pull()
+    st.info("ML info")
+    st.dataframe(df)
     best_model = compare_models()
     compare_df = pull()
+    st.info("Comparision table of ML models")
+    st.dataframe(compare_df)
     save_model(best_model,'best_model')
     return([setup_df,compare_df,best_model])
 
